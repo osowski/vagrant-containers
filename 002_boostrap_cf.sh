56ff14ad3fe5a0897b47f89852767cc87a837e37
Rick Osowski <(none)>
2017-02-06 17:49:18 -0500

    Updated CF & Bluemix CLI plugins
    Signed-off-by: Rick Osowski <(none)>

--- 002_boostrap_cf.sh
+++ 002_boostrap_cf.sh
@@ -1,16 +1,21 @@
 !#/bin/bash
 
+#Install CF
 wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
 echo "deb http://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
 sudo apt-get update
 sudo apt-get install cf-cli jq
 
-#TODO Install Bluemix CLI
+#Install Bluemix CLI
 CURRENT_BX_VERSION=$(curl -s http://clis.ng.bluemix.net/all_versions | jq -r .[0].version)
 curl --output Bluemix_CLI_${CURRENT_BX_VERSION}_amd64.tar.gz http://public.dhe.ibm.com/cloud/bluemix/cli/bluemix-cli/Bluemix_CLI_${CURRENT_BX_VERSION}_amd64.tar.gz
 tar -xvf Bluemix_CLI_${CURRENT_BX_VERSION}_amd64.tar.gz
 cd Bluemix_CLI
 sudo ./install_bluemix_cli
+
+#Install Plugins
+export CF_HOME=/home/vagrant
+export CF_PLUGIN_HOME=/home/vagrant
 
 bx plugin install auto-scaling -r Bluemix
 bx plugin install active-deploy -r Bluemix
@@ -27,3 +32,5 @@ cf uninstall-plugin IBM-Containers
 cf install-plugin -f https://static-ice.ng.bluemix.net/ibm-containers-linux_x64
 
 cf api https://api.ng.bluemix.net
+
+chown -R vagrant:vagrant /home/vagrant/.cf
